b20bc27f7e859b44f68cb92a36e9ec4cd53e882a
Sonia Carolina Blanco D <soniacarolina.blanco@gmail.com>
2019-04-07 21:00:16 +0100

    replacing classes by anonymous classes

--- app/src/main/java/soni/example/loli_counter/MainActivity.kt
+++ app/src/main/java/soni/example/loli_counter/MainActivity.kt
@@ -2,6 +2,7 @@ package soni.example.loli_counter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,16 +10,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val papa = Dad()
-        val mama = Mom()
+
         val loli = Loli()
-        loli.addSubscriptor(papa)
-        loli.addSubscriptor(mama)
+        loli.addSubscriptor(object: Subscriptor<Int>{
+            override fun action(value: Int) {
+                Log.d("New number",value.toString())
+            }
+        })
+        loli.addSubscriptor(object: Subscriptor<Int>{
+            override fun action(value: Int) {
+                Log.d("Multiplied by 2 ",(value*2).toString())
+            }
+        })
 
         val cat = Cat()
 
+        cat.addSubscriptor(object: Subscriptor<String>{
+            override fun action(value:String){
+                Log.d("cat says ",value)
+            }
+        })
+
         button.setOnClickListener {
             loli.count()
+            cat.meow()
         }
     }
 }
